1deafc3b3c6bd74b6375f2c7d83ff19632ef4fcb
ViralSir <viralsir2018@gmail.com>
2024-02-12 09:10:00 +0530

    how to get input from url and pass it to html page that functionality has been implemented.

--- hello/urls.py
+++ hello/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns=[
-    path('home/', Home, name="hello-home"),
+    path('home/<str:name>', Home, name="hello-home"),
     path("about/", Aboutus, name="hello-aboutus"),
     path("contactus/", Contactus, name="hello-contactus")
 
--- hello/views.py
+++ hello/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def Home(request):
+def Home(request,name):
   #  return HttpResponse("<h1>Hello</h1>");
-  return render(request,"hello/home.html")
+  return render(request,"hello/home.html",{"username":name})
 
 def Aboutus(request):
    return render(request,"hello/Aboutus.html")
